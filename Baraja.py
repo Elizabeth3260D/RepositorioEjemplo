cc69c6d8e49694f893287990d26386ab2c29d2dd
Elizabeth <dominguezduran.eliza@gmail.com>
2021-05-11 19:46:15 -0600

    Programa de inicio v1

---
+++ Baraja.py
@@ -0,0 +1,8 @@
+listaNumeros=["A","1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+listaFiguras=["Corazon Rojo","Corazon Negro","Diamante Rojo","Trebol Negro"]
+
+
+for x in range(4):
+  for y in range(14):
+    print(listaNumeros[y]+" "+listaFiguras[x])
